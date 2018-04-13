8484a77f829d9e99493bfca5afe22ce29d87da06
Katherine Door <kittydoordev@gmail.com>
2018-04-13 13:37:18 +0200

    reverse-local ssh alias

--- zsh/.config/zsh/ssh_hosts.zsh
+++ zsh/.config/zsh/ssh_hosts.zsh
@@ -4,3 +4,4 @@
 alias kitty.sh="ssh kitty@kitty.sh"
 alias nyaa-core="ssh kitty@nyaa-core.kitty.sh -p 22335"
 alias kitty.sh-reverse="ssh -R 2222:localhost:22 -N kitty@kitty.sh"
+alias reverse-local="ssh kitty@localhost -p 2222"
