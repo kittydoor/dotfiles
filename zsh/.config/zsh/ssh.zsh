deab9e65633d666eecd6ea9cd13087cf5293b7c5
Katherine Pata <katherine@podictive.com>
2018-06-19 22:43:32 +0200

    Removed github key add by default

--- zsh/.config/zsh/ssh.zsh
+++ zsh/.config/zsh/ssh.zsh
@@ -1,13 +1,8 @@
-function git-ssh {
-  ssh-add ~/.ssh/id_rsa_github
-}
-
 # start ssh-agent
 if ! (pidof ssh-agent > /dev/null); then
   ssh-agent > ~/.ssh-agent.env
   if [[ -f ~/.ssh/id_rsa_github  ]] then
     eval "$(<~/.ssh-agent.env)" > /dev/null
-    ssh-add ~/.ssh/id_rsa_github
   fi
 fi
 
