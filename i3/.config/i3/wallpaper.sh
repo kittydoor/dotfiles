eed5c41304f55f2210e513c4fee17c8fbd2cb7a5
Katherine Door <kittydoordev@gmail.com>
2018-07-31 09:06:15 +0200

    Fixed wallpaper.sh

--- i3/.config/i3/wallpaper.sh
+++ i3/.config/i3/wallpaper.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [ which hsetroot 1>/dev/null 2>&1 ]; then
+if which hsetroot 1>/dev/null 2>&1; then
   hsetroot -solid "#000000"
-elif [ which xsetroot 1>/dev/null 2>&1 ]; then
+elif which xsetroot 1>/dev/null 2>&1; then
   xsetroot -solid "#000000"
 fi
