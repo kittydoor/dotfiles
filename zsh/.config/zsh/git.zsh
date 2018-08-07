3d9fd29480e08efdd04ff5d960336b498668624b
Katherine Pata <katherine@podictive.com>
2018-08-07 16:24:00 +0200

    Added pod dir to hashed dirs

--- zsh/.config/zsh/git.zsh
+++ zsh/.config/zsh/git.zsh
@@ -10,5 +10,6 @@ zstyle ':vcs_info:git:*' formats '%b'
 
 hash -d git=~/Documents/Git
 hash -d vmaas=~git/VMaaS
+hash -d pod=~/Documents/Podictive
 
 alias gl="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset'"
