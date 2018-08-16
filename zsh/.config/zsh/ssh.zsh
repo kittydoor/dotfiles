70da79e24be3e3ccb08758a3d2a823f8e38c7330
Katherine Pata <katherine@podictive.com>
2018-08-16 14:15:44 +0200

    Not starting ssh-agent if gnome-keyring already in place

--- zsh/.config/zsh/ssh.zsh
+++ zsh/.config/zsh/ssh.zsh
@@ -1,12 +1,15 @@
-# start ssh-agent
-if ! (pidof ssh-agent > /dev/null); then
-  ssh-agent > ~/.ssh-agent.env
-  if [[ -f ~/.ssh/id_rsa_github  ]] then
+# if no gnome-keyring
+if [[ -z "$(ps -e | grep gnome-keyring)" ]]; then
+  # start ssh-agent
+  if ! (pidof ssh-agent > /dev/null); then
+    ssh-agent > ~/.ssh-agent.env
+    if [[ -f ~/.ssh/id_rsa_github  ]] then
+      eval "$(<~/.ssh-agent.env)" > /dev/null
+    fi
+  fi
+
+  # on new shells
+  if [[ "$SSH_AGENT_PID" == "" ]]; then
     eval "$(<~/.ssh-agent.env)" > /dev/null
   fi
-fi
-
-# on new shells
-if [[ "$SSH_AGENT_PID" == "" ]]; then
-  eval "$(<~/.ssh-agent.env)" > /dev/null
-fi
+fi;
