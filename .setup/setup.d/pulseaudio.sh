2a3724c710c6a5089738e21ad3af4a3d9571c7cb
Katherine Pata <katherine@podictive.com>
2018-11-04 03:51:28 +0100

    setup.d folder, with the first setup script which is for PulseAudio

---
+++ .setup/setup.d/pulseaudio.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Setup Script for PulseAudio
+pacman -S \
+	pavucontrol \
+	pulseaudio-alsa \
+	pulseaudio
+
+# pulseaudio - core package
+# -alsa - proxy for programs that don't support pa
+# pavucontrol - PA Volume Control
