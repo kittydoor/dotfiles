bb819802d6495c2eaf48dd1b61422013bc9f5320
kittydoor <kittydoordev@gmail.com>
2018-12-09 00:49:11 +0000

    A lot of improvements on 2

--- .setup/notes/2-main-install.sh
+++ .setup/notes/2-main-install.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
+set -o errexit
+set -o nounset
+set -o xtrace
+set -o pipefail
 
 strap_packages () {
-  pacstrap /mnt base # can also append here
+  pacstrap /mnt --needed base zsh # can also append here
 }
 
 configure_fstab () {
-  cat << EOF > /mnt/etc/fstab
+  cat <<"EOF" > /mnt/etc/fstab
 # Static information about the filesystems.
 # See fstab(5) for details.
 
@@ -15,16 +19,33 @@ EOF
 }
 
 chroot_and_config () {
-  cat << EOF | arch-chroot /mnt bash
+  cat <<"EOF" | arch-chroot /mnt bash
+set -o errexit
+set -o nounset
+set -o xtrace
+set -o pipefail
+
+# time
 ln -sf /usr/share/zoneinfo/Europe/Amsterdam /etc/localtime
 hwclock --systohc
+
+# locale
 sed -i 's/#en_US.UTF-8/en_US.UTF-8/' /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
+
+# hostname
 # nyaa-core, remote, gate, proxy, company-name
 echo nyaa-aigent > /etc/hostname
-passwd
+
+# users
+id -u kitty || useradd -m -G users,wheel -s /bin/zsh kitty # lazy eval for useradd
+cat <<"EOF" | chpasswd -e
+root:$6$acKP91SbTv0uqzmP$BB4gBLdfWYjWGWX8GWHETJyxMcOo7eX433ePJaXsvXja8.T0IkYYUns0DTzTn5bMhVg.43R76Icbf3TiHRgXe/
+kitty:$6$QZBx6bYoJJUgUkN6$KwUTLc8M4LkTBhEiajdWgJ.qv3zk9pgvZwgpAAlIFGWYItlilosL/zydx8w335TF1RNiYDDC0qkVrtOuma6pA1
 EOF
 }
 
+strap_packages
+configure_fstab
 chroot_and_config
