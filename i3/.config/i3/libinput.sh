8a7d95f4c7ccb8148763552a26a1dd2dbd81bd22
kittydoor <kittydoordev@gmail.com>
2018-12-09 15:40:15 +0100

    libinput.sh now also supports work laptop

--- i3/.config/i3/libinput.sh
+++ i3/.config/i3/libinput.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+setup_touchpad () {
+  xinput set-prop "$1" "libinput Tapping Enabled" 1
+  xinput set-prop "$1" "libinput Natural Scrolling Enabled" 1
+}
+
 if [[ "$(hostname)" == "nyaa-gate" ]]; then
-  xinput set-prop "DLL082A:01 06CB:76AF Touchpad" "libinput Tapping Enabled" 1
-  xinput set-prop "DLL082A:01 06CB:76AF Touchpad" "libinput Natural Scrolling Enabled" 1
+  setup_touchpad "DLL082A:01 06CB:76AF Touchpad"
+elif [[ "$(hostname)" == "nyaa-aigent" ]]; then
+  setup_touchpad "DELL081C:00 044E:121F Touchpad"
 fi
