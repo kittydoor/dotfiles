d17a341cbcebf46a0fb1d2bd969232fabb381e74
kittydoor <kittydoordev@gmail.com>
2018-12-09 12:33:51 +0000

    Fixed and/or precedence, added crypttab setup

--- .setup/notes/3-setup-bootloader-and-kernel.sh
+++ .setup/notes/3-setup-bootloader-and-kernel.sh
@@ -15,20 +15,29 @@ pacman -S --needed grub efibootmgr
 
 # check file is one of expected states
 grep "^HOOKS=(base udev autodetect keyboard modconf block encrypt lvm2 filesystems fsck)$" /etc/mkinitcpio.conf ||
-  grep "^HOOKS=(base udev autodetect modconf block filesystems keyboard fsck)$" /etc/mkinicpio.conf &&
-  sed -i "s/^HOOKS=([a-z ]*)$/HOOKS=(base udev autodetect keyboard modconf block encrypt lvm2 filesystems fsck)/" /etc/mkinitcpio.conf
+  {
+    grep "^HOOKS=(base udev autodetect modconf block filesystems keyboard fsck)$" /etc/mkinitcpio.conf &&
+    sed -i "s/^HOOKS=([a-z ]*)$/HOOKS=(base udev autodetect keyboard modconf block encrypt lvm2 filesystems fsck)/" /etc/mkinitcpio.conf
+  }
 # TODO: Make the code actually add the options to any state, rather than only this one
 
 mkinitcpio -p linux
 grep "^GRUB_ENABLE_CRYPTODISK=y$" /etc/default/grub ||
-  grep "^#GRUB_ENABLE_CRYPTODISK=[yn]$" /etc/default/grub &&
-  sed -i 's/^#GRUB_ENABLE_CRYPTODISK=[yn]/GRUB_ENABLE_CRYPTODISK=y/' /etc/default/grub
+  {
+    grep "^#GRUB_ENABLE_CRYPTODISK=[yn]$" /etc/default/grub &&
+    sed -i 's/^#GRUB_ENABLE_CRYPTODISK=[yn]/GRUB_ENABLE_CRYPTODISK=y/' /etc/default/grub
+  }
 
 grep "^GRUB_CMDLINE_LINUX=\"cryptdevice=UUID=$(blkid -s UUID -o value /dev/sda3):cryptlvm\"" /etc/default/grub ||
-  grep "^GRUB_CMDLINE_LINUX=\"\"$"  /etc/default/grub &&
-  sed -i "s/^GRUB_CMDLINE_LINUX=\"\"/GRUB_CMDLINE_LINUX=\"cryptdevice=UUID=$(blkid -s UUID -o value /dev/sda3):cryptlvm\"/" /etc/default/grub
+  {
+    grep "^GRUB_CMDLINE_LINUX=\"\"$"  /etc/default/grub &&
+    sed -i "s/^GRUB_CMDLINE_LINUX=\"\"/GRUB_CMDLINE_LINUX=\"cryptdevice=UUID=$(blkid -s UUID -o value /dev/sda3):cryptlvm\"/" /etc/default/grub
+  }
 # TODO: Make the code above add the option no matter previous configured state, rather than only if empty
 # TODO: Does this evaluate blkid in the heredoc or in the chroot?
+
+grep -P "^cryptboot\t/dev/sda3\tnone\tluks$" /etc/crypttab ||
+  echo -e "cryptboot\t/dev/sda3\tnone\tluks" >> /etc/crypttab
 
 grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
