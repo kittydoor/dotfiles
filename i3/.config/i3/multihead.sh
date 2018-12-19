cf4e65340a0c40a868bb1564b8e229e2d763fe23
Katherine Door <kittydoordev@gmail.com>
2018-12-19 17:41:07 +0100

    Improved multihead script

--- i3/.config/i3/multihead.sh
+++ i3/.config/i3/multihead.sh
@@ -4,28 +4,57 @@ set -o nounset
 set -o pipefail
 set -o xtrace
 
+nyaa_core_single () {
+  xrandr --output HDMI-2 --off --output HDMI-1 --primary --mode 2560x1440 --pos 0x0 --rotate normal
+}
+
+nyaa_core_dual () {
+  xrandr --output HDMI-2 --mode 1920x1080 --pos 2560x0 --rotate normal --output HDMI-1 --primary --mode 2560x1440 --pos 0x0 --rotate normal
+}
+
+nyaa_aigent_single () {
+  xrandr --output VIRTUAL1 --off --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output HDMI2 --off --output HDMI1 --off --output DP2 --off
+}
+
+nyaa_aigent_dual () {
+  xrandr --output VIRTUAL1 --off --output eDP1 --primary --mode 1920x1080 --pos 0x360 --rotate normal --output DP1 --mode 2560x1440 --pos 1920x0 --rotate normal --output HDMI2 --off --output HDMI1 --off --output DP2 --off
+}
+
+nyaa_gate_single () {
+  exit 2
+}
+
+nyaa_gate_dual () {
+  exit 2
+}
+
 case "$(hostname) ${1:-single}" in
 "nyaa-core single")
-  xrandr --output HDMI-2 --auto --off, mode "default"
+  nyaa_core_single
   ;;
-"nyaa-core double")
-  xrandr --output HDMI-2 --auto --right-of HDMI-1, mode "default"
+"nyaa-core dual")
+  nyaa_core_dual
   ;;
 "nyaa-core default")
-  xrandr --output HDMI-2 --auto --right-of HDMI-1, mode "default"
+  nyaa_core_dual
   ;;
 "nyaa-aigent single")
-  xrandr --output VIRTUAL1 --off --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output HDMI2 --off --output HDMI1 --off --output DP2 --off
+  nyaa_aigent_single
   ;;
-"nyaa-aigent double")
-  xrandr --output VIRTUAL1 --off --output eDP1 --primary --mode 1920x1080 --pos 0x360 --rotate normal --output DP1 --mode 2560x1440 --pos 1920x0 --rotate normal --output HDMI2 --off --output HDMI1 --off --output DP2 --off
+"nyaa-aigent dual")
+  nyaa_aigent_dual
   ;;
 "nyaa-aigent default")
-  xrandr --output VIRTUAL1 --off --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output HDMI2 --off --output HDMI1 --off --output DP2 --off
+  nyaa_aigent_single
   ;;
-"nyaa-remote single")
+"nyaa-gate single")
+  nyaa_gate_single
   ;;
-"nyaa-remote double")
+"nyaa-gate dual")
+  nyaa_gate_dual
+  ;;
+"nyaa-gate default")
+  nyaa_gate_single
   ;;
 *)
   exit 1
