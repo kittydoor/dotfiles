1712bc46469836ec148cf3d5d4c8aa29ddb9b66b
Katherine Pata <katherine@podictive.com>
2019-01-08 13:53:59 +0100

    Commented out xsetroot as it doesn't work with compton

--- i3/.config/i3/wallpaper.sh
+++ i3/.config/i3/wallpaper.sh
@@ -6,6 +6,6 @@ set -o xtrace
 
 if which hsetroot 1>/dev/null 2>&1; then
   hsetroot -solid "#000000"
-elif which xsetroot 1>/dev/null 2>&1; then
-  xsetroot -solid "#000000"
+#elif which xsetroot 1>/dev/null 2>&1; then
+#  xsetroot -solid "#000000"
 fi
