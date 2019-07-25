44c1233596d1b1e5b747b122cd52e6085a50a5e9
Katherine Pata <katherine@aigent.com>
2019-07-25 10:46:49 +0200

    Proper quoting in prompt var

--- .config/zsh/git.zsh
+++ .config/zsh/git.zsh
@@ -2,7 +2,7 @@ autoload -Uz vcs_info
 precmd_vcs_info() { vcs_info }
 precmd_functions+=( precmd_vcs_info )
 setopt prompt_subst
-PROMPT=\$vcs_info_msg_0_\ $PROMPT
+PROMPT="\$vcs_info_msg_0_ ${PROMPT}"
 zstyle ':vcs_info:git:*' formats '%b'
 
 ### Aliases
