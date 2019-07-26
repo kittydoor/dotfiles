4c942dbedf6e8d177219e222be4b649230cc8e75
Katherine Pata <katherine@aigent.com>
2019-07-26 14:44:01 +0200

    Fully functional url retrieval system for clone_group.py, actual cloning functionality not yet implemented

--- bin/clone_group.py
+++ bin/clone_group.py
@@ -1,7 +1,11 @@
+#!/usr/bin/env python3
 import os
-import urllib
+import logging
+# from pprint import pprint
 
 import requests
+
+logging.basicConfig(level=logging.WARNING)
 
 base_url = "https://gitlab.com"
 api_suffix = "/api/v4"
@@ -21,35 +25,66 @@ with open(os.path.expanduser("~/.gitlab")) as token_file:
     token_key = token_file.read().strip()
 token = token_prefix + token_key
 
+
 def get_resource(query):
+    logging.info("Getting a resource")
     response = requests.get(url + '/' + query + token)
     if response.status_code != 200:
-        return None
+        print(response.text)
+        exit(1)
     return response.json()
 
+
 def get_group(group_id):
-    return get_resource("groups/" + str(group_id))
+    logging.info(f"Get group { group_id }")
+    data = get_resource("groups/" + str(group_id))
+    logging.info(f"Retrieved { data['full_path'] }")
+    return data
+
 
 def get_subgroups_ids(group_id):
+    logging.info("Get subgroups ids")
     subgroups = get_resource("groups/" + str(group_id) + "/subgroups")
-    return [ subgroup["id"] for subgroup in subgroups ]
+    return [subgroup["id"] for subgroup in subgroups]
+
 
 def get_subgroups(group_id):
-    return [ get_group(subgroup_id)
-            for subgroup_id in get_subgroups_ids(group_id) ]
+    logging.info("Get subgroups")
+    subgroups = [get_group(subgroup_id)
+                 for subgroup_id in get_subgroups_ids(group_id)]
+    logging.info("Got subgroups: "
+                 f"{ [ subgroup['full_path'] for subgroup in subgroups ] }")
+    return subgroups
+
 
 def retrieve_projects(group):
-    return [ project[link_type] for project in group["projects"] ]
+    logging.info("Retrieve project")
+    retrieved_projects = [project[link_type] for project in group["projects"]]
+    logging.info(f"Retrieved projects:\n{ retrieved_projects }")
+    return retrieved_projects
+
 
 def get_projects_recursive(group_id):
+    logging.info("Getting projects recursive call")
     projects = []
     for subgroup in get_subgroups(group_id):
-        projects.append(retrieve_projects(subgroup))
-        projects.append(get_projects_recursive(group_id))
+        logging.info(f"Checking subgroup { subgroup['full_path'] }")
+        projects.extend(retrieve_projects(subgroup))
+        projects.extend(get_projects_recursive(subgroup['id']))
     return projects
 
-def get_projects(group_id):
-    projects = retrieve_projects(get_group(group_id))
-    projects.append(get_projects_recursive(group_id))
 
-get_projects(groups["test"])
+def get_projects(group_id):
+    logging.info("Getting projects")
+    projects = retrieve_projects(get_group(group_id))
+    new_projects = get_projects_recursive(group_id)
+    if new_projects:
+        projects.extend(new_projects)
+    return projects
+
+
+root_group = list(groups.values())[0]
+logging.info(f"Checking group with id {root_group}")
+projects = get_projects(root_group)
+
+print(*projects, sep="\n")
