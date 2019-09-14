6dee69fb5066af17cb63597635ba48bc3327c574
kittydoor <kittydoordev@gmail.com>
2019-09-14 15:19:05 +0200

    Gnome-aware ssh

--- .config/zsh/ssh.zsh
+++ .config/zsh/ssh.zsh
@@ -1,9 +1,17 @@
 # For other agents: if [[ "$SSH_AUTH_SOCK" == "" ]]; then
 
-# start ssh-agent
-if ! pidof ssh-agent > /dev/null; then
-  ssh-agent > ~/.ssh/agent_env
+# Start ssh agent
+if [[ "${DESKTOP_SESSION}" = gnome-DISABLED ]]; then
+  #if ! pidof gnome-keyring-daemon > /dev/null; then
+    gnome-keyring-daemon --start > ~/.ssh/agent_env
+  #fi
+else
+  if ! pidof ssh-agent > /dev/null; then
+    ssh-agent > ~/.ssh/agent_env
+  fi
 fi
 
 # on new shells
 eval "$(<~/.ssh/agent_env)" > /dev/null
+# gnome-keyring-daemon doesn't export by default
+export SSH_AUTH_SOCK
