96034522def8d49a4acfdddab5a1261d6d0daa9f
kittydoor <me@kitty.sh>
2019-09-27 18:01:33 +0200

    better pacman.sh (stow wrapper) and some improvements to software role

--- packages/bin-dir/bin/pacman.sh
+++ packages/bin-dir/bin/pacman.sh
@@ -1,7 +1,48 @@
 #!/bin/bash
+set -euo pipefail
+if [[ ${DEBUG:-} = 1 ]]; then
+  set -o xtrace
+fi
+
+echoerr() {
+  echo -n -e "\e[34m!Error:\e[39m " > /dev/stderr
+  echo "$@" > /dev/stderr
+}
+
+# read file path, get the directory name, go up a few steps, and then resolve the relative path
+SCRIPT_DIR="$(readlink -m "$(dirname "$(readlink -f "$0")")/../../..")"
+STOW_ARGS=("--dir=${SCRIPT_DIR}/packages/" "--target=/home/${USER}")
+
+help_message() {
+  echoerr "No package names given."
+  echo 'Use "all" to install all packages.'
+  echo
+  echo 'Available packages:' > /dev/stderr
+  for dir in "${SCRIPT_DIR}"/packages/*; do
+    if [[ -d "$dir" ]]; then
+      echo "  $(basename "${dir}")"
+    fi
+  done
+}
+
 if [[ -z "${1:-}" ]]; then
-  echo "No package names given." > /dev/stderr
+  help_message
   exit 1
 fi
 
-stow --dir="packages/" --target="/home/${USER}" -S "$@"
+if [[ "${1}" == "all" ]]; then
+  for dir in "${SCRIPT_DIR}"/packages/*; do
+    if [[ -d $dir ]]; then
+      stow "${STOW_ARGS[@]}" "$(basename "$dir")"
+    else
+      echo "This should not happen. There is a non-directory file in the packages directory."
+      exit 2
+    fi
+  done
+else
+  for item in "$@"; do
+    if [[ -d "${SCRIPT_DIR}/packages/${item}" ]]; then
+      stow "${STOW_ARGS[@]}" "$item"
+    fi
+  done
+fi
