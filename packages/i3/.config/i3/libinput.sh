11c5e0b18abb66a0c53d271be598dde230192918
kittydoor <kittydoordev@gmail.com>
2019-10-30 18:28:23 +0100

    Changed hostname to generic nyaa-work for touchpad switch

--- packages/i3/.config/i3/libinput.sh
+++ packages/i3/.config/i3/libinput.sh
@@ -11,6 +11,6 @@ setup_touchpad () {
 
 if [[ "$(hostname)" == "nyaa-gate" ]]; then
   setup_touchpad "DLL082A:01 06CB:76AF Touchpad"
-elif [[ "$(hostname)" == "nyaa-aigent" ]]; then
+elif [[ "$(hostname)" == "nyaa-work" ]]; then
   setup_touchpad "DELL081C:00 044E:121F Touchpad"
 fi
