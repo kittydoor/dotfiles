c8014e3c5f3d4e647118f4705cef2bdbe7857dc5
kittydoor <kittydoordev@gmail.com>
2019-10-30 14:08:38 +0100

    New and improved volume control script

--- packages/i3/.config/i3/volume.sh
+++ packages/i3/.config/i3/volume.sh
@@ -1,39 +1,137 @@
 #!/bin/bash
-set -o errexit
-set -o nounset
-set -o pipefail
-set -o xtrace
+set -euo pipefail
 
-if ! which pactl 1>/dev/null 2>&1 ; then
+help_message() {
+  echo 'Volume control helper script'
+  echo
+  echo "Usage: $0 <option> [inc/dec amount]"
+  echo '          set <percentage>'
+  echo
+  echo 'Options:'
+  echo '  inc     increments audio by percentage'
+  echo '  dec     decrements audio by percentage'
+  echo '  set     sets audio to percentage'
+  echo '  toggle  toggles audio mute'
+  echo '  mute    mutes audio'
+  echo
+  echo 'inc and dec snap to the next multiple of 5 if no amount provided'
+}
+
+echoerr() {
+  >&2 echo -n -e "\e[34m!Error:\e[39m "
+  >&2 echo "$@"
+}
+
+if ! type pactl >/dev/null; then
+  echoerr "'pactl' command not found. This script depends on pactl."
   exit 1
 fi;
 
-log="volume.sh"
-if [[ $1 == "toggle" ]]; then
-  log+=" toggling mute"
+if [[ -z ${1:-} ]]; then
+  echoerr "No arguments given"
+  help_message
+  exit 1
+fi
+
+check_parse() {
+  #if ! [[ $1 =~ ^(100\|[[1-9][0-9]\|[9-0])$ ]]; then
+  if ! [[ $1 =~ ^([1-9][0-9]|[0-9])$ ]]; then
+    echoerr "Amount given is not a valid integer (e.g. 50)"
+    exit 1
+  fi
+}
+
+inc_vol() {
+  pactl set-sink-volume @DEFAULT_SINK@ "+$1%"
+  smart_mute
+}
+
+dec_vol() {
+  pactl set-sink-volume @DEFAULT_SINK@ "-$1%"
+  smart_mute
+}
+
+inc_snap() {
+  VOLUME_STR=$(current_volume)
+  VOLUME=${VOLUME_STR%\%}
+  DELTA="$(( 5 - (VOLUME % 5) ))"
+  inc_vol "${DELTA}"
+}
+
+dec_snap() {
+  VOLUME_STR=$(current_volume)
+  VOLUME=${VOLUME_STR%\%}
+  DELTA="$(( VOLUME % 5 ))"
+  if [[ ${DELTA} == 0 ]]; then
+    dec_vol 5
+  else
+    dec_vol "${DELTA}"
+  fi
+}
+
+set_vol() {
+  pactl set-sink-volume @DEFAULT_SINK@ "$1%"
+  smart_mute
+}
+
+toggle_vol() {
   pactl set-sink-mute @DEFAULT_SINK@ toggle
-  echo -e $log;
-  exit
-elif [[ $1 == "inc" ]]; then
-  log+=" incrementing by"
-elif [[ $1 == "dec" ]]; then
-  log+=" decrementing by"
-else
-  log="Error. Incorrect arguments."
-  exit 2;
-fi;
+}
 
-if [[ $2 =~ ^[1-9][0-9]?$ ]]; then
-  log+=" $2%\n"
-else
-  exit 2;
-fi;
+mute_vol() {
+  pactl set-sink-mute @DEFAULT_SINK@ 1
+}
 
-pactl set-sink-mute 0 false
-if [[ $1 == "inc" ]]; then
-  pactl set-sink-volume @DEFAULT_SINK@ "+$2%"
-elif [[ $1 == "dec" ]]; then
-  pactl set-sink-volume @DEFAULT_SINK@ "-$2%"
-fi;
+current_volume() {
+  pactl list sinks | awk '/Volume: / { print $5 }' | head -n 1
+}
 
-echo -e $log;
+smart_mute() {
+  if [[ $(current_volume) == 0% ]]; then
+    pactl set-sink-mute @DEFAULT_SINK@ 1
+  else
+    pactl set-sink-mute @DEFAULT_SINK@ 0
+  fi
+}
+
+
+case "${1:-}" in
+  "inc")
+    shift
+    if [[ -z ${1:-} ]]; then
+      inc_snap
+    else
+      check_parse "$1"
+      inc_vol "$1"
+    fi
+    ;;
+  "dec")
+    shift
+    if [[ -z ${1:-} ]]; then
+      dec_snap
+    else
+      check_parse "$1"
+      dec_vol "$1"
+    fi
+    ;;
+  "set")
+    shift
+    if [[ -z ${1:-} ]]; then
+      echoerr "No percentage provided for set"
+      exit 1
+    else
+      check_parse "$1"
+      set_vol "$1"
+    fi
+    ;;
+  "toggle")
+    toggle_vol
+    ;;
+  "mute")
+    mute_vol
+    ;;
+  *)
+    echoerr "$1 is not a valid command"
+    help_message
+    exit 1
+esac
