880f39906a988bd3ba50f89a26ac15d41913b01e
kittydoor <kittydoordev@gmail.com>
2019-11-20 18:27:08 +0100

    kubectl lazyload completion

--- packages/terminal/.config/zsh/completion.d/kubectl.zsh
+++ packages/terminal/.config/zsh/completion.d/kubectl.zsh
@@ -1,4 +1,12 @@
-if [ $commands[kubectl] ]; then
-  # Should be "source <(kubectl completion zsh)", but it fails atm
-  eval $(kubectl completion zsh)
-fi
+# Lazy load because kubectl completion sucks
+# https://github.com/kubernetes/kubernetes/issues/59078
+function kubectl() {
+  if ! type __start_kubectl > /dev/null 2>&1; then
+    # Fails with 127, see https://github.com/kubernetes/kubectl/issues/125
+    source <(command kubectl completion zsh)\
+      && echo "kubectl completion seems fixed, update zsh config"\
+      || true
+  fi
+
+  command kubectl "$@"
+}
