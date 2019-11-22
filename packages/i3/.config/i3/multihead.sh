06a81a149bde5e3917d023509d6f63a76bc1977a
kittydoor <kittydoordev@gmail.com>
2019-11-22 15:09:56 +0100

    Unified swaymsg commands into one in multihead.sh

--- packages/i3/.config/i3/multihead.sh
+++ packages/i3/.config/i3/multihead.sh
@@ -12,21 +12,17 @@ nyaa_work_x11_dual() {
   xrandr --output HDMI-2 --off --output HDMI-1 --off --output DP-1 --mode 2560x1440 --pos 1920x0 --rotate normal --output eDP-1 --primary --mode 1920x1080 --pos 0x360 --rotate normal --output DP-2 --off
 }
 
-nyaa_work_wayland_setup(){
-  MAIN='output eDP-1 pos 0 360 res 1920x1080 bg #000000 solid_color'
-  SECONDARY='output DP-1 pos 1920 0 res 3840x2160 bg #000000 solid_color scale 1.5'
-  swaymsg "${MAIN}; ${SECONDARY}; focus output eDP-1"
-}
+NYAA_WORK_WAYLAND_MAIN='output eDP-1 pos 0 360 res 1920x1080 bg #000000 solid_color'
+NYAA_WORK_WAYLAND_SECONDARY='output DP-1 pos 1920 0 res 3840x2160 bg #000000 solid_color scale 1.5'
+NYAA_WORK_WAYLAND_SETUP="${NYAA_WORK_WAYLAND_MAIN}; ${NYAA_WORK_WAYLAND_SECONDARY}; focus output eDP-1"
 
 nyaa_work_wayland_single() {
-  nyaa_work_wayland_setup
-  #TODO: Figure out why the command below prints tons of newlines
-  swaymsg 'output eDP-1 enable; output DP-1 disable' > /dev/null
+  # TODO: Figure out why the command below prints tons of newlines
+  swaymsg "${NYAA_WORK_WAYLAND_SETUP}; output eDP-1 enable; output DP-1 disable" > /dev/null
 }
 
 nyaa_work_wayland_dual() {
-  nyaa_work_wayland_setup
-  swaymsg 'output eDP-1 enable; output DP-1 enable' > /dev/null
+  swaymsg "${NYAA_WORK_WAYLAND_SETUP}; output eDP-1 enable; output DP-1 enable" > /dev/null
 }
 
 nyaa_work_wayland_default() {
