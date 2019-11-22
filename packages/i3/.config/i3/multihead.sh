5db8ba625496cdaed3736490674fa1b7a453ef7b
kittydoor <kittydoordev@gmail.com>
2019-11-22 13:51:33 +0100

    Changed multihead.sh to be more dynamic, and support sway

--- packages/i3/.config/i3/multihead.sh
+++ packages/i3/.config/i3/multihead.sh
@@ -4,59 +4,37 @@ set -o nounset
 set -o pipefail
 set -o xtrace
 
-nyaa_core_single () {
-  echo nyi
-}
-
-nyaa_core_dual () {
-  echo nyi
-}
-
-nyaa_work_single () {
+nyaa_work_x11_single() {
   xrandr --output HDMI-2 --off --output HDMI-1 --off --output DP-1 --off --output eDP-1 --primary --mode 1920x1080 --pos 0x360 --rotate normal --output DP-2 --off
 }
 
-nyaa_work_dual () {
+nyaa_work_x11_dual() {
   xrandr --output HDMI-2 --off --output HDMI-1 --off --output DP-1 --mode 2560x1440 --pos 1920x0 --rotate normal --output eDP-1 --primary --mode 1920x1080 --pos 0x360 --rotate normal --output DP-2 --off
 }
 
-nyaa_gate_single () {
-  exit 2
+nyaa_work_wayland_setup(){
+  MAIN='output eDP-1 pos 0 360 res 1920x1080 bg #000000 solid_color'
+  SECONDARY='output DP-1 pos 1920 0 res 3840x2160 bg #000000 solid_color scale 1.5'
+  swaymsg "${MAIN}; ${SECONDARY}; focus output eDP-1"
 }
 
-nyaa_gate_dual () {
-  exit 2
+nyaa_work_wayland_single() {
+  nyaa_work_wayland_setup
+  #TODO: Figure out why the command below prints tons of newlines
+  swaymsg 'output eDP-1 enable; output DP-1 disable' > /dev/null
 }
 
-case "$(hostname) ${1:-single}" in
-"nyaa-core single")
-  nyaa_core_single
-  ;;
-"nyaa-core dual")
-  nyaa_core_dual
-  ;;
-"nyaa-core default")
-  nyaa_core_dual
-  ;;
-"nyaa-work single")
-  nyaa_work_single
-  ;;
-"nyaa-work dual")
-  nyaa_work_dual
-  ;;
-"nyaa-work default")
-  nyaa_work_single
-  ;;
-"nyaa-gate single")
-  nyaa_gate_single
-  ;;
-"nyaa-gate dual")
-  nyaa_gate_dual
-  ;;
-"nyaa-gate default")
-  nyaa_gate_single
-  ;;
-*)
-  exit 1
-  ;;
-esac
+nyaa_work_wayland_dual() {
+  nyaa_work_wayland_setup
+  swaymsg 'output eDP-1 enable; output DP-1 enable' > /dev/null
+}
+
+nyaa_work_wayland_default() {
+  nyaa_work_wayland_single
+}
+
+COMMAND="${HOSTNAME//-/_}_${XDG_SESSION_TYPE:-}_${1:-default}"
+
+if [[ $(type -t "${COMMAND}") == function ]]; then
+  ${COMMAND}
+fi
