1b767396015403a17b80f011c17582a80c107966
kittydoor <kittydoordev@gmail.com>
2019-12-12 11:42:31 +0100

    Fixed ssh agent issue with new agents being spawned on new terminal

--- packages/common/.config/zsh/ssh.zsh
+++ packages/common/.config/zsh/ssh.zsh
@@ -1,18 +1,29 @@
+# TODO: Warning: At least on Sway, this file is sourced on the first opened terminal,
+# so SSH_AUTH_SOCK isn't set and inherited by default.
+
 # Check if SSH_AUTH_SOCK is not a valid socket
 if [[ ! -S "${SSH_AUTH_SOCK}" ]]; then
-  # Then, launch agent
-  # Otherwise, return
+  # If it is not a valid socket, check if there's an agent_env with a valid socket
+  # If it is a valid socket, do nothing
 
-  if [[ "${DESKTOP_SESSION}" = gnome ]]; then
-    # If running on gnome, use gnome tooling
-    gnome-keyring-daemon --start > ~/.ssh/agent_env
-    # Gnome doesn't add this line by default
-    echo "export SSH_AUTH_SOCK" >> ~/.ssh/agent_env
+  if [[ -S "$(source ~/.ssh/agent_env > /dev/null; echo $SSH_AUTH_SOCK)" ]]; then
+    # If agent_env has valid socket, source it
+    source ~/.ssh/agent_env > /dev/null
+
   else
-    # If not running gnome, use universal (for ssh or other de/wm)
-    ssh-agent > ~/.ssh/agent_env
-  fi
+    # Launch new agent and overwrite agent_env
 
-  # Evaluate agent vars after having started an agent
-  eval "$(<~/.ssh/agent_env)" > /dev/null
+    if [[ "${DESKTOP_SESSION}" = gnome ]]; then
+      # If running on gnome, use gnome tooling
+      gnome-keyring-daemon --start > ~/.ssh/agent_env
+      # Gnome doesn't add this line by default
+      echo "export SSH_AUTH_SOCK" >> ~/.ssh/agent_env
+    else
+      # If not running gnome, use universal (for ssh or other de/wm)
+      ssh-agent > ~/.ssh/agent_env
+    fi
+
+    # Evaluate agent vars after having started an agent
+    source ~/.ssh/agent_env > /dev/null
+  fi
 fi
