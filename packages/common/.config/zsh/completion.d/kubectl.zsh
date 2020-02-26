f3d77fdec163af86ed2bcc3dc0fa3ade57f2c9d8
kittydoor <kittydoordev@gmail.com>
2020-02-26 10:19:08 +0100

    Check available kubeconfig files on every kubectl

--- packages/common/.config/zsh/completion.d/kubectl.zsh
+++ packages/common/.config/zsh/completion.d/kubectl.zsh
@@ -1,6 +1,9 @@
 # Lazy load because kubectl completion sucks
 # https://github.com/kubernetes/kubernetes/issues/59078
+
 function kubectl() {
+  export KUBECONFIG=$(find ~/.kube/ -name 'config_*' -type f -printf '%p:' | sed 's/:$//')
+
   if ! type __start_kubectl > /dev/null 2>&1; then
     # Fails with 127, see https://github.com/kubernetes/kubectl/issues/125
     source <(command kubectl completion zsh)\
