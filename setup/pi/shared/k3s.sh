e283d6d281264b1ff76e565a58c8c324b428fd96
kittydoor <kittydoordev@gmail.com>
2020-07-01 23:49:00 +0200

    Some notes and pi scratch files

--- setup/pi/shared/k3s.sh
+++ setup/pi/shared/k3s.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -euo pipefail
+set -o xtrace
 
 ssh nyaa-pi4.lan bash -s <<EOF
 curl -sfL https://get.k3s.io | sh -s - --write-kubeconfig-mode 644
@@ -12,4 +14,8 @@ scp nyaa-pi4.lan:/etc/rancher/k3s/k3s.yaml ~/.kube/config_pi4
 
 if grep default ~/.kube/config_pi4 > /dev/null; then
   sed -i 's/default/pi4/g' ~/.kube/config_pi4
+fi
+
+if grep 127.0.0.1 ~/.kube/config_pi4 > /dev/null; then
+  sed -i 's/127.0.0.1/nyaa-pi4.lan/' ~/.kube/config_pi4
 fi
