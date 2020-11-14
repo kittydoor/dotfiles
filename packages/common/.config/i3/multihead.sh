5974a300b9f40ed3460152552ba0221f38ce96dc
kittydoor <me@kitty.sh>
2020-11-14 20:43:06 +0100

    Fixed i3 nyaa-core display config

--- packages/common/.config/i3/multihead.sh
+++ packages/common/.config/i3/multihead.sh
@@ -4,30 +4,44 @@ set -o nounset
 set -o pipefail
 set -o xtrace
 
-nyaa_work_x11_single() {
-  xrandr --output HDMI-2 --off --output HDMI-1 --off --output DP-1 --off --output eDP-1 --primary --mode 1920x1080 --pos 0x360 --rotate normal --output DP-2 --off
+# NYAA-CORE
+
+nyaa_core_x11_single() {
+  xrandr --output HDMI-1 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output HDMI-2 --off
 }
 
-nyaa_work_x11_dual() {
-  xrandr --output HDMI-2 --off --output HDMI-1 --off --output DP-1 --mode 2560x1440 --pos 1920x0 --rotate normal --output eDP-1 --primary --mode 1920x1080 --pos 0x360 --rotate normal --output DP-2 --off
+nyaa_core_x11_dual() {
+  xrandr --output HDMI-1 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output HDMI-2 --mode 1920x1080 --pos 2560x360 --rotate normal
 }
 
-NYAA_WORK_WAYLAND_MAIN='output eDP-1 pos 0 360 res 1920x1080 bg #000000 solid_color'
-NYAA_WORK_WAYLAND_SECONDARY='output DP-1 pos 1920 0 res 3840x2160 bg #000000 solid_color scale 1.5'
-NYAA_WORK_WAYLAND_SETUP="${NYAA_WORK_WAYLAND_MAIN}; ${NYAA_WORK_WAYLAND_SECONDARY}; focus output eDP-1"
-
-nyaa_work_wayland_single() {
-  # TODO: Figure out why the command below prints tons of newlines
-  swaymsg "${NYAA_WORK_WAYLAND_SETUP}; output eDP-1 enable; output DP-1 disable" > /dev/null
+nyaa_core_x11_alternate() {
+  xrandr --output HDMI-1 --off --output HDMI-2 --primary --mode 1920x1080 --pos 0x0 --rotate normal
 }
 
-nyaa_work_wayland_dual() {
-  swaymsg "${NYAA_WORK_WAYLAND_SETUP}; output eDP-1 enable; output DP-1 enable" > /dev/null
+nyaa_core_x11_default() {
+  nyaa_core_x11_dual
 }
 
-nyaa_work_wayland_default() {
-  nyaa_work_wayland_single
-}
+# NYAA-GATE
+# TODO: Add config
+
+# Old work setup for wayland, nowadays using include for system specific sway configs
+# NYAA_WORK_WAYLAND_MAIN='output eDP-1 pos 0 360 res 1920x1080 bg #000000 solid_color'
+# NYAA_WORK_WAYLAND_SECONDARY='output DP-1 pos 1920 0 res 3840x2160 bg #000000 solid_color scale 1.5'
+# NYAA_WORK_WAYLAND_SETUP="${NYAA_WORK_WAYLAND_MAIN}; ${NYAA_WORK_WAYLAND_SECONDARY}; focus output eDP-1"
+#
+# nyaa_work_wayland_single() {
+#   # TODO: Figure out why the command below prints tons of newlines
+#   swaymsg "${NYAA_WORK_WAYLAND_SETUP}; output eDP-1 enable; output DP-1 disable" > /dev/null
+# }
+#
+# nyaa_work_wayland_dual() {
+#   swaymsg "${NYAA_WORK_WAYLAND_SETUP}; output eDP-1 enable; output DP-1 enable" > /dev/null
+# }
+#
+# nyaa_work_wayland_default() {
+#   nyaa_work_wayland_single
+# }
 
 COMMAND="${HOSTNAME//-/_}_${XDG_SESSION_TYPE:-}_${1:-default}"
 
