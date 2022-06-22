7acf8a59e857a1278105276b8a05330f80f92da2
kittydoor <me@kitty.sh>
2022-06-22 02:53:08 +0200

    Don't prematurely exit scripts/lint.sh

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+# shellcheck disable=SC2038
 set -euo pipefail
 
+# Store exit codes and exit at the end in order to run all linters
+EXIT_CODE=0
+
 # Find bashisms in scripts with shebang #!/bin/sh
-find . -path './.git' -prune -o -type f -exec awk 'NR==1&&/^#!\/bin\/sh$/{print FILENAME}' {} \; | xargs checkbashisms
+find . \( -path './.git' -o -path './deprecated' \) -prune -o -type f -exec awk 'NR==1&&/^#!\/bin\/sh$/{print FILENAME}' {} \; | xargs checkbashisms || EXIT_CODE=1
 
 # Shellcheck all files that start with a shebang of #!/bin/*sh (e.g. sh, bash, zsh)
-find . -path './.git' -prune -o -type f -exec awk 'NR==1&&/^#!\/bin\/.*sh$/{print FILENAME}' {} \; | xargs shellcheck
+find . \( -path './.git' -o -path './deprecated' \) -prune -o -type f -exec awk 'NR==1&&/^#!\/bin\/.*sh$/{print FILENAME}' {} \; | xargs shellcheck || EXIT_CODE=1
+
+exit "${EXIT_CODE}"
