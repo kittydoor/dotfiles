2b55380bfd69b041707d31f7ec9612dc5d3166e7
kittydoor <me@kitty.sh>
2022-07-19 00:29:45 +0200

    Removed aliases from zsh config, since now using .gitconfig for it

--- packages/common/.config/zsh/git.zsh
+++ packages/common/.config/zsh/git.zsh
@@ -6,17 +6,9 @@ precmd_functions+=( precmd_vcs_info )
 setopt prompt_subst
 PROMPT="\${vcs_info_msg_0_}${PROMPT}"
 
-### Aliases
+### Alias
 
-alias gs="git status "
-alias gco="git checkout "
-alias gcm="git commit "
-alias ga="git add "
-alias gd="git diff "
-alias gdc="git diff --cached "
-alias gl="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' "
-alias gp="git pull "
-alias gpr="git pull --rebase "
+alias g="git"
 
 ### Static Named Directories
 
