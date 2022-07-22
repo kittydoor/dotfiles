6f7b95969c68436004eed893026aa89b43e0b5e3
kittydoor <me@kitty.sh>
2022-07-22 17:11:49 +0200

    Add zsh function for presentation mode

--- packages/common/.config/zsh/functions.zsh
+++ packages/common/.config/zsh/functions.zsh
@@ -10,3 +10,17 @@ ranger() {
     exit
   fi
 }
+
+ORIG_PROMPT="${PROMPT}"
+ORIG_RPROMPT="${RPROMPT}"
+zsh-presentation-mode() {
+  if [[ -z "${ZSH_PRESENTATION_MODE}" ]]; then
+    ZSH_PRESENTATION_MODE=1
+    PROMPT="$ "
+    RPROMPT=""
+  else
+    ZSH_PRESENTATION_MODE=""
+    PROMPT="${ORIG_PROMPT}"
+    RPROMPT="${ORIG_RPROMPT}"
+  fi
+}
