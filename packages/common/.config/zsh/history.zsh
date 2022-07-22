a7d8521186036dcb9322c7c56b24c477c75bf60d
kittydoor <me@kitty.sh>
2022-07-22 17:12:22 +0200

    zsh: don't set INC_APPEND_HISTORY when SHARE_HISTORY is already set

--- packages/common/.config/zsh/history.zsh
+++ packages/common/.config/zsh/history.zsh
@@ -5,12 +5,13 @@ HISTFILE=~/.zsh_history
 HISTSIZE=12000 # internal hist size
 SAVEHIST=10000 # file hist size
 
-setopt INC_APPEND_HISTORY        # Write to the history file immediately, not when the shell exits.
 setopt SHARE_HISTORY             # Share history between all sessions.
 setopt HIST_EXPIRE_DUPS_FIRST    # Expire duplicate entries first when trimming history.
 setopt HIST_FIND_NO_DUPS         # Do not display a line previously found.
 setopt HIST_IGNORE_SPACE         # Don't record an entry starting with a space.
 setopt HIST_VERIFY               # Don't execute immediately upon history expansion.
+
+# setopt INC_APPEND_HISTORY        # Write to the history file immediately, not when the shell exits.
 # setopt BANG_HIST                 # Treat the '!' character specially during expansion.
 # setopt EXTENDED_HISTORY          # Write the history file in the ":start:elapsed;command" format.
 # setopt HIST_IGNORE_DUPS          # Don't record an entry that was just recorded again.
